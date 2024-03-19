698c097e18a1a0bc39127d1748b3be9cfb2f5e50
Sofía Velásquez <98661058+Sofiamishel2003@users.noreply.github.com>
2024-03-19 15:52:56 -0600

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,6 @@ module.exports= {
     apps:[{
         name: 'blog sofia',
         script: 'npm',
-        args: 'start',
-        log_file:'./blog_dennis.txt',
-        merge_logs:true
+        args: 'start'
     }]
 }
