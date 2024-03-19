6ba15a8658fd5dcda507a89b8d62262949b72ca4
Sofiamishel2003 <Sofiamishel2003@gmail.com>
2024-03-18 20:21:40 -0600

    Add Ecosystem for server

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports= {
+    apps:[{
+        name: 'blog sofia',
+        script: 'npm',
+        args: 'start',
+        log_file:'./blog_dennis.txt',
+        merge_logs:true
+    }]
+}
