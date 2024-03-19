3fdf1dc383b8778caf2d8cc926ffb4a7e56bcc74
Sofía Velásquez <98661058+Sofiamishel2003@users.noreply.github.com>
2024-03-19 16:39:28 -0600

    add sofia database of the server to conn

--- conn.js
+++ conn.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise'
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'sofiamishell2003',
-    database: 'blog_db',
+    database: 'blog_sofia',
     password: '1234',
     waitForConnections: true,
     connectionLimit: 10,
